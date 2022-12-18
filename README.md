8c274c8a3f2678134156ea585f541bb48c0ae858
csnv <jorgecsnova@gmail.com>
2022-12-19 00:04:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Mindu
+Web server written in Node.js for the Hercules emulator.
+## Requisites
+- Nodejs
+- csnv's webtoken plugin
+- [This PR](https://github.com/HerculesWS/Hercules/pull/3183)
---
+++ index.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response, NextFunction } from 'express';
+import router from './routes';
+import Config from './config/webserver';
+import multer from 'multer';
+import { DatabaseMigration } from './utils/database-migration';
+
+const upload = multer();
+const app = express();
+
+DatabaseMigration.init();
+
+/**
+ * Parse multipart/form-data to routes
+ */
+app.use("/", upload.any(), (req: Request, res: Response, next: NextFunction) => {
+    next();
+})
+
+/**
+ * API routes
+ */
+app.use(router);
+
+/**
+ * Server setup and listening port
+ */
+app.listen(Config.web.port, () => {
+    console.log(`Webserver running on port ${Config.web.port}`);
+});
